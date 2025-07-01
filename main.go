069212ed69ee67578b7d5f81c8de35c8c2365546
TheIndra55 <theindra@protonmail.com>
2025-07-01 22:35:50 +0200

    Add support for newer semantic location history format

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"path"
 	"sort"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/charmbracelet/log"
@@ -142,18 +143,18 @@ func processFile(fileName string, startDate, endDate time.Time, officeLocation o
 	placesProcessed := 0
 
 	for _, place := range places {
-		if place.Duration.End.Before(startDate) || place.Duration.Start.After(endDate) {
+		if place.End.Before(startDate) || place.Start.After(endDate) {
 			// We expect entries to be in sorted order, so we could break here.
 			// But as we do not know for sure we instead go the extra mile.
 			continue
 		}
 
-		loc := orb.Point{float64(place.CenterLatE7) / 1e7, float64(place.CenterLngE7) / 1e7}
+		loc := orb.Point{place.Latitude, place.Longitude}
 
 		distance := geo.DistanceHaversine(officeLocation, loc)
 
 		if distance <= tolerance {
-			daysInTheOffice.Add(place.Duration.Start)
+			daysInTheOffice.Add(place.Start)
 		}
 
 		placesProcessed++
@@ -195,11 +196,13 @@ func listFilesRecursively(inputDir string) ([]string, error) {
 	return list, nil
 }
 
-func ParseTimelineInput(input io.Reader) ([]*timelineVisitedPlace, error) {
+func ParseTimelineInput(input io.Reader) ([]timelinePoint, error) {
 	type wrapper struct {
 		TimelineObjects []struct {
 			PlaceVisit *timelineVisitedPlace `json:"placeVisit"`
 		} `json:"timelineObjects"`
+
+		SemanticSegments []semanticSegment `json:"semanticSegments"`
 	}
 
 	var w wrapper
@@ -208,13 +211,33 @@ func ParseTimelineInput(input io.Reader) ([]*timelineVisitedPlace, error) {
 		return nil, fmt.Errorf("decoding JSON: %w", err)
 	}
 
+	var result []timelinePoint
+
+	// Check for the newer semantic location history format exported from local device
+	if w.SemanticSegments != nil {
+		for _, entry := range w.SemanticSegments {
+			for _, point := range entry.TimelinePath {
+				// Parse the point
+				lat, long := parsePoint(point.Point)
+
+				result = append(result, timelinePoint{
+					Latitude:  lat,
+					Longitude: long,
+					Start:     entry.StartTime,
+					End:       entry.EndTime,
+				})
+			}
+		}
+
+		return result, nil
+	}
+
 	// Remove nil entries, i.e. entries that are not place visits but activity segments or something else
-	var result []*timelineVisitedPlace
 	for _, entry := range w.TimelineObjects {
 		if entry.PlaceVisit == nil {
 			continue
 		}
-		
+
 		// Google removed these two fields at some point, so we simply take the second best option.
 		// See below.
 		if entry.PlaceVisit.CenterLatE7 == 0 || entry.PlaceVisit.CenterLngE7 == 0 {
@@ -222,10 +245,35 @@ func ParseTimelineInput(input io.Reader) ([]*timelineVisitedPlace, error) {
 			entry.PlaceVisit.CenterLngE7 = entry.PlaceVisit.Location.LongitudeE7
 		}
 
-		result = append(result, entry.PlaceVisit)
+		place := entry.PlaceVisit
+
+		result = append(result, timelinePoint{
+			Latitude:  float64(place.CenterLatE7) / 1e7,
+			Longitude: float64(place.CenterLngE7) / 1e7,
+			Start:     place.Duration.Start,
+			End:       place.Duration.End,
+		})
 	}
 
 	return result, nil
+}
+
+func parsePoint(value string) (float64, float64) {
+	// "51.6503959°, 5.0492413°"
+	coords := strings.Split(strings.ReplaceAll(value, "°", ""), ", ")
+
+	lat, _ := strconv.ParseFloat(coords[0], 64)
+	long, _ := strconv.ParseFloat(coords[1], 64)
+
+	return lat, long
+}
+
+type timelinePoint struct {
+	Latitude  float64
+	Longitude float64
+
+	Start time.Time
+	End   time.Time
 }
 
 type timelineVisitedPlace struct {
@@ -244,4 +292,13 @@ type timelineVisitedPlace struct {
 	// after this date.
 	CenterLatE7 int `json:"centerLatE7"`
 	CenterLngE7 int `json:"centerLngE7"`
+}
+
+type semanticSegment struct {
+	StartTime    time.Time `json:"startTime"`
+	EndTime      time.Time `json:"endTime"`
+	TimelinePath []struct {
+		Point string    `json:"point"`
+		Time  time.Time `json:"time"`
+	} `json:"timelinePath"`
 }
